15eadb4c7d0eca009bf0e72e4a9d578d44480206
william-ly <william.ly01@gmail.com>
2016-11-18 22:06:12 -0500

    test update

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace DiscordBot
             _client = new DiscordClient();
             DateTime start = new DateTime();
             start = DateTime.Now;
-            Console.WriteLine("BOTmoderator online!");
+            Console.WriteLine("BOTmoderator!");
 
             //Add command service
             _client.UsingCommands(x => {
